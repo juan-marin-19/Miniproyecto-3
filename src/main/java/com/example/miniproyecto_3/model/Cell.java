6623794c893c229974983e7c8c623274233e361a
Isabela Bermudez <isabela.bermudez@correounivalle.edu.co>
2025-05-07 18:57:10 -0500

    Added functions to the Cell class

--- src/main/java/com/example/miniproyecto_3/model/Cell.java
+++ src/main/java/com/example/miniproyecto_3/model/Cell.java
@@ -6,4 +6,33 @@ public class Cell {
     private boolean shot  = false;
     private Ship ship = null;
 
+    public Cell(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    public int getColumn() {
+        return column;
+    }
+    public boolean isShot() {
+        return shot;
+    }
+    public void setShot(){
+        this.shot = true;
+    }
+    public Ship getShip() {
+        return ship;
+    }
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
+    public boolean isFull(){
+        if(ship != null){
+            return true;
+        }
+        return false;
+    }
 }
