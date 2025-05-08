6f587df4d40a26076550a5b8e814c49dd505c018
Isabela Bermudez <isabela.bermudez@correounivalle.edu.co>
2025-05-07 19:22:49 -0500

    attributes and constructor of the ship class have been added

--- src/main/java/com/example/miniproyecto_3/model/Ship.java
+++ src/main/java/com/example/miniproyecto_3/model/Ship.java
@@ -1,4 +1,18 @@
 package com.example.miniproyecto_3.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ship {
+    private String type;
+    private int size;
+    private List<Cell> locations;
+    private List<Cell> cellsTouched;
+
+    public Ship(String type, int size){
+        this.type = type;
+        this.size = size;
+        this.locations = new ArrayList<>();
+        this.cellsTouched = new ArrayList<>();
+    }
 }
