99e0401598cbb6ec0ed8ba639a6aa53bfa75bca6
Isabela Bermudez <isabela.bermudez@correounivalle.edu.co>
2025-05-07 21:00:50 -0500

    constructor and setShip function were added to the board class

--- src/main/java/com/example/miniproyecto_3/model/Board.java
+++ src/main/java/com/example/miniproyecto_3/model/Board.java
@@ -1,5 +1,6 @@
 package com.example.miniproyecto_3.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
@@ -7,4 +8,48 @@ public class Board {
     private final int columns = 10;
     private List<List<Cell>> board;
     private List<Ship> ships;
+
+    public Board() {
+        board = new ArrayList<>();
+        ships = new ArrayList<>();
+
+        //initialize the list of list
+        for (int i = 0; i < rows; i++) {
+            List<Cell> rowCells = new ArrayList<>();
+            for (int j = 0; j < columns; j++) {
+                rowCells.add(new Cell(i, j));
+            }
+            board.add(rowCells);
+        }
+    }
+
+    public boolean setShip(Ship ship, int initialRow, int initialColumn, boolean isHorizontal) {
+        List<Cell> positions = new ArrayList<>();
+        for (int i = 0; i < ship.getSize(); i++) {
+            int row, column = 0;
+            if (isHorizontal) {
+                row = initialRow;
+                column = initialColumn + i;
+            } else {
+                row = initialRow + i;
+                column = initialColumn;
+            }
+            if (row >= rows && column >= columns) {
+                return false;
+            }
+
+            Cell finalCell = board.get(row).get(column);
+            if (finalCell.isFull()) {
+                return false;
+            }
+
+            positions.add(finalCell);
+        }
+        for (Cell finalCell : positions) {
+            finalCell.setShip(ship);
+            ship.addLocation(finalCell);
+        }
+        ships.add(ship);
+        return true;
+    }
 }
