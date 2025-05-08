d16266b50d75dd6946fe1e8db3ccc5eeb87bf005
Isabela Bermudez <isabela.bermudez@correounivalle.edu.co>
2025-05-07 19:53:49 -0500

    Attributes were added to the class board

--- src/main/java/com/example/miniproyecto_3/model/Board.java
+++ src/main/java/com/example/miniproyecto_3/model/Board.java
@@ -1,4 +1,10 @@
 package com.example.miniproyecto_3.model;
 
+import java.util.List;
+
 public class Board {
+    private final int rows = 10;
+    private final int columns = 10;
+    private List<List<Cell>> board;
+    private List<Ship> ships;
 }
