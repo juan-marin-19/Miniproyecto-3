413ac4d281e15cb83937838a6134a688e208fdd2
Isabela Bermudez <isabela.bermudez@correounivalle.edu.co>
2025-05-09 17:42:17 -0500

    new functions were added to the board class

--- src/main/java/com/example/miniproyecto_3/model/Board.java
+++ src/main/java/com/example/miniproyecto_3/model/Board.java
@@ -52,4 +52,42 @@ public class Board {
         ships.add(ship);
         return true;
     }
+
+    //Clase interna con constantes de resultado
+    public static class Result {
+        public static final String AGUA  = "AGUA";
+        public static final String TOCADO = "TOCADO";
+        public static final String HUNDIDO = "HUNDIDO";
+    }
+
+    public String ResultShot(int row, int column) {
+        Cell c = board.get(row).get(column);
+        if(c.isFull()) {
+            return "Ya disparaste aquí";
+        }
+        c.setShot();
+        if(c.getShip() != null) {
+            Ship Shotship = c.getShip();
+            Shotship.setCellsTouched(c);
+            return Shotship.isSunkenShip() ? Result.HUNDIDO : Result.TOCADO;
+        }
+        return Result.AGUA;
+    }
+
+    public boolean allTheSunkenShips(){
+        for(Ship ship : ships) {
+            if(!ship.isSunkenShip()) {
+                return false;
+            }
+        } return true;
+    }
+
+    public Cell getCell(int row, int column) {
+        return board.get(row).get(column);
+    }
+
+    public List<Ship> getShips() {
+        return ships;
+    }
+
 }
