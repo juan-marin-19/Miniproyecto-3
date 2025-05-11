d1346b843d73dc83fcce468978c19c8542b3a2d0
Juan Marin Orozco <juanmarinorozco6@gmail.com>
2025-05-10 23:32:44 -0500

    plain files added

--- src/main/java/com/example/miniproyecto_3/controller/GameController.java
+++ src/main/java/com/example/miniproyecto_3/controller/GameController.java
@@ -4,10 +4,14 @@ import com.example.miniproyecto_3.model.Player;
 
 public class GameController {
 
+    private Player player;
+
     public GameController() {
         // No debe tener parámetros
     }
     public void startPlay(Player player){
-
+        this.player = player;
+        System.out.println(player.getSunkenShips());
+        System.out.println(player.getNickname());
     }
 }
--- src/main/java/com/example/miniproyecto_3/controller/WelcomeController.java
+++ src/main/java/com/example/miniproyecto_3/controller/WelcomeController.java
@@ -14,20 +14,25 @@ public class WelcomeController {
 
     private PlainTextFileReader plainTextFileReader;
 
+    private PlainTextFileReader plainTextFileReader2;
+
     @FXML
     private TextField nickname;
 
      public void initialize() {
     this.plainTextFileReader = new PlainTextFileReader();
+    this.plainTextFileReader2 = new PlainTextFileReader();
      }
 
 
     @FXML
     public void handleClickPlay(javafx.event.ActionEvent event) {
         if (!nickname.getText().isEmpty()) {
-            Player player = new Player(nickname.getText(), 0);
-            WelcomeStage.deleteInstance();
+            Player player = new Player(nickname.getText().trim(), 0);
+            String content = player.getNickname() + "," + player.getSunkenShips();
+            plainTextFileReader.writeToFile("player_data.csv",content);
 
+            WelcomeStage.deleteInstance();
             // Crea una nueva instancia de GameStage
             GameStage gameStage = new GameStage();
 
@@ -46,4 +51,29 @@ public class WelcomeController {
         }
     }
 
+    @FXML
+    public void handleClickContinue(javafx.event.ActionEvent event) {
+        String[] data = plainTextFileReader2.readFromFile("player_data.csv");
+
+        String nickname = data[0];
+        int sunkenBoats = Integer.parseInt(data[1]);
+        Player player = new Player(nickname, sunkenBoats);
+        // Player player = (Player) serializableFileHandler.deserialize("player_data.ser");
+
+        WelcomeStage.deleteInstance();
+        // Crea una nueva instancia de GameStage
+        GameStage gameStage = new GameStage();
+
+        // Asegúrate de que el controlador no sea null antes de llamar startPlay
+
+        //AQUI VA UN TRY CATCH PARA EL GAMECONTROLLER NULL
+
+        GameController controller = gameStage.getGameController();
+        if (controller != null) {
+            controller.startPlay(player);
+        } else {
+            System.err.println("Error: GameController is null");
+        }
+
+    }
 }
